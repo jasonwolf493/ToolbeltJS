3905a719cf4e2ef39baa7ea5aa4cbc7c434b3d97
Jason Wolf <jasonwolf493@gmail.com>
2016-03-07 03:18:07 -0500

    opencart

---
+++ ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://10.0.0.119:8888/includes/plugins/opencart-master/opencart-master/upload/admin/');
+define('HTTP_CATALOG', 'http://10.0.0.119:8888/includes/plugins/opencart-master/opencart-master/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://10.0.0.119:8888/includes/plugins/opencart-master/opencart-master/upload/admin/');
+define('HTTPS_CATALOG', 'http://10.0.0.119:8888/includes/plugins/opencart-master/opencart-master/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/admin/');
+define('DIR_SYSTEM', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/');
+define('DIR_IMAGE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/image/');
+define('DIR_LANGUAGE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/admin/language/');
+define('DIR_TEMPLATE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/admin/view/template/');
+define('DIR_CONFIG', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/config/');
+define('DIR_CACHE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/upload/');
+define('DIR_CATALOG', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://10.0.0.119:8888/includes/plugins/opencart-master/opencart-master/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://10.0.0.119:8888/includes/plugins/opencart-master/opencart-master/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/catalog/');
+define('DIR_SYSTEM', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/');
+define('DIR_IMAGE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/image/');
+define('DIR_LANGUAGE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/config/');
+define('DIR_CACHE', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/Jason/Documents/GitHub/ToolbeltJS/ToolbeltJS/includes/plugins/opencart-master/opencart-master/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
